705b28ae9171d4c8ad4cd6c8ab7ca3945347546d
Patel-Pal <patelpal.93130@gmail.com>
2025-11-15 22:10:36 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://protfolio-fe.vercel.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
